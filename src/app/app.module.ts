acfc28b337b94a4e8ef77c181d3a29ed7be108bc
mahmoudshahin1111@gmail.com <mahmoudshahin1111@gmail.com>
2021-10-09 21:09:20 +0200

    udpate example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Units } from 'projects/ngx-network/src/lib/api/units';
 import { NgxNetworkModule } from 'projects/ngx-network/src/public-api';
 
 import { AppComponent } from './app.component';

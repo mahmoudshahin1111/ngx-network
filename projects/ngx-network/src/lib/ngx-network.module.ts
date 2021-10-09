75c92b5c41765d3e628c7ef80a89ec5d48180033
mahmoudshahin1111@gmail.com <mahmoudshahin1111@gmail.com>
2021-10-09 20:22:10 +0200

    + remove unused component

--- projects/ngx-network/src/lib/ngx-network.module.ts
+++ projects/ngx-network/src/lib/ngx-network.module.ts
@@ -1,33 +1,23 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
-import { NgxNetworkComponent } from './ngx-network.component';
-import {HttpClientModule} from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 import { NgxNetworkService } from './ngx-network.service';
 import { Config } from './api/config';
 
 export const NGX_NETWORK_CONFIG = 'NGX_NETWORK_CONFIG';
 @NgModule({
-  declarations: [
-    NgxNetworkComponent
-  ],
-  imports: [
-    HttpClientModule
-  ],
-  exports: [
-    NgxNetworkComponent,
-  ]
+  imports: [HttpClientModule],
 })
-export class NgxNetworkModule { 
-  static forRoot(config:Config):ModuleWithProviders<NgxNetworkModule>{
-    return{
-      ngModule:NgxNetworkModule,
-      providers:[
+export class NgxNetworkModule {
+  static forRoot(config: Config): ModuleWithProviders<NgxNetworkModule> {
+    return {
+      ngModule: NgxNetworkModule,
+      providers: [
         NgxNetworkService,
         {
-          provide:NGX_NETWORK_CONFIG,
-          useValue:config
-        }
-
-      ]
-    }
+          provide: NGX_NETWORK_CONFIG,
+          useValue: config,
+        },
+      ],
+    };
   }
 }
